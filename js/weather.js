2683cbf1b11913e889fc6e0f693466c84c785cc2
wang_si_eun <alo201011@gmail.com>
2023-04-02 19:27:09 +0900

    # 날씨, 위치 정보 추가

--- js/weather.js
+++ js/weather.js
@@ -1,13 +1,26 @@
+const API_KEY = "8500fe23fc65036167057cb033cd6165";
+
 function onGeoOk(position){
     // 날씨, 위치 정보 받아오기
     const lat = position.coords.latitude;
-    const lng = position.coords.longityde;
-    console.log(lat, lng);
+    const lon = position.coords.longityde;
+
+    const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&lang={lang}`
+    // const url = `https://api.openweathermap.org/data/2.5/weather?lat=37.598&lon=126.9783&appid=8500fe23fc65036167057cb033cd6165&lang={lang}`ß;
+    
+    fetch(url)
+        .then(responce => reponse.json())
+        .then(data =>{
+        const weather = document.querySelector("#weather-box span:first-child");
+        const city = document.querySelector("#weather-box span:last-child");
+
+        city.innerText = data.name;
+        weather.innerText = data.weather[0].main;
+    });
 }
 
 function onGeoError(){
-
+    alert("Can't find you. No weather for you.");
 }
-
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
