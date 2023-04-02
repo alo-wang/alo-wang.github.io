b1674354c1ed850d30e092739bb80e9040108e8e
wang_si_eun <alo201011@gmail.com>
2023-04-02 18:54:22 +0900

    # 위치 테스트 추가

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,8 @@
 function onGeoOk(position){
     // 날씨, 위치 정보 받아오기
+    const lat = position.coords.latitude;
+    const lng = position.coords.longityde;
+    console.log(lat, lng);
 }
 
 function onGeoError(){
@@ -7,4 +10,4 @@ function onGeoError(){
 }
 
 
-// navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
