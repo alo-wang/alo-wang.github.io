7c73a4de3aa97f0ed5266a643aaf82d34a6b912c
왕시은 <alo201011@gmail.com>
2023-04-24 18:24:39 +0900

    [JION] 7차구성(배경및 컬러변경/ active 유지)

--- js/jion.js
+++ js/jion.js
@@ -3,8 +3,7 @@ const flipText = document.querySelectorAll('figure p');
 const flipBtn = document.querySelectorAll('figure figcaption');
 
 flipBtn.forEach(element => {
-    console.log(element)
-    element.addEventListener('click',randomText);
+    // element.addEventListener('click',randomText);
     randomText();
 });
 
@@ -13,7 +12,7 @@ function flipClass(item){
     flipImg.forEach(function(item){
         item.addEventListener('click',function(){
             flipImg.forEach(function(e){
-                e.classList.remove('active'); 
+                //e.classList.remove('active'); 
             });
             item.classList.add('active');
         }); 
