88bd048a19dcef730ad38b68d26b8e920d7e3805
wang_si_eun <alo201011@gmail.com>
2023-04-25 16:43:54 +0900

    [JION] 랜덤 숫자 중복 제어 수정

--- js/jion.js
+++ js/jion.js
@@ -2,9 +2,10 @@ const flipImg = document.querySelectorAll('figure');
 const flipText = document.querySelectorAll('figure p');
 const flipBtn = document.querySelectorAll('figure figcaption');
 
+selectIndex(10,10)
+
 flipBtn.forEach(element => {
-    // element.addEventListener('click',randomText);
-    randomText();
+    flipClass();
 });
 
 // flip(class list)
@@ -19,40 +20,19 @@ function flipClass(item){
     });
 }
 
-// random event
-function randomText(){
-    let randomIndexArray = [];
-    const randomNumber = [1,2,3,4,5,6,7,8,9,10];
-    
+// set random number
+function selectIndex(totalIndex, selectingNumber){
+    let randomIndexArray = []
+    for (i=0; i<selectingNumber; i++) {
+        let randomNum = Math.floor(Math.random() * totalIndex + 1);
 
-    // test중
-    // https://velog.io/@eunjin/Javascript-%ED%8A%B9%EC%A0%95-%EC%88%AB%EC%9E%90-%EB%B2%94%EC%9C%84%EC%97%90%EC%84%9C-%EC%A4%91%EB%B3%B5%EC%9D%84-%EC%A0%9C%EC%99%B8%ED%95%9C-%EB%9E%9C%EB%8D%A4-%EC%88%AB%EC%9E%90-%EB%BD%91%EB%8A%94-%EB%B0%A9%EB%B2%95
-    flipText.forEach(element => {
-        for (i = 0; i < randomNumber.length; i++) {   //check if there is any duplicate index
-            let randomNum = Math.floor(Math.random() * randomNumber.length);
-            
-            //console.log(randomNumber.indexOf(randomNum))
-            if (randomNumber.indexOf(randomNum) === -1) {
-                // randomNumber.push(randomNum)
-                element.innerHTML = `<span>축하해요!</span>` + randomNum;
-            } else { //if the randomNum is already in the array retry
-              i--
-            }
+        if (randomIndexArray.indexOf(randomNum) === -1) {
+            randomIndexArray.push(randomNum)
+            flipText[i].innerHTML = `<span>축하해요!</span>` + randomIndexArray[i];
+            // console.log('test',flipText[i].innerHTML)
+        } else { 
+            i--
         }
-
-    });
-
-
-    // return
-
-    /*
-    flipText.forEach(element => {
-        // element.innerHTML = `<span>축하해요!</span>` + Math.ceil(Math.random() * 10);
-
-        element.innerHTML = `<span>축하해요!</span>` + randomNumber[Math.floor(Math.random() * randomNumber.length)];
-        
-    });
-    */
-    flipClass();
+    }
+    return randomIndexArray
 }
-
