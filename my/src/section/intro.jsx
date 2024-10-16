e1ace513d3f3bf867ed4cb7ffe9ae36172104ed1
왕시은 <alo201011@gmail.cpm>
2024-10-16 23:35:55 +0900

    [Ready] 인트로영역 작업중

---
+++ my/src/section/intro.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import introImg from '../img/img-intro.png'
+
+function Intro(props){
+    return(
+        <section className="section_intro">
+            <h1>대웅과 시은<br/>결혼합니다</h1>
+            <img src={introImg} alt="인트로 이미지"></img>
+            <p>2024. 12. 08 | 11:10 <span>라비니움 1층 리츄얼홀</span></p>
+        </section>
+    );
+}
+
+export default Intro;
