db201c4990e2f70a931ccdaac518dbda09c68654
왕시은 <alo201011@gmail.cpm>
2024-10-24 00:50:36 +0900

    [Ready] 인트로 완료

--- my/src/section/greeting.jsx
+++ my/src/section/greeting.jsx
@@ -71,16 +71,15 @@ function Greeting(props){
             </div>
 
             <div className="greeting-btm">
+                <p className="btm-txt">
+                    가족이 만나<br/>가족이 되다.
+                </p>
                 <div className="bottom">
                     <figure>
                         <img src={familyImgU01} alt="쥬스를 마시는 시은과 커피를 마시는 대웅의 모습"></img>
                     </figure>
                 </div>
-                <p>
-                    가족이 만나<br/>가족이 되다.
-                </p>
-
-                <p>
+                <p className="btm-txt">
                     2024.12.08<br/>대웅, 시은<br/>가족이 되다.
                 </p>
 
@@ -88,7 +87,7 @@ function Greeting(props){
                     <img src={familyImgU02} alt="다정하게 포즈를 취하는 4컷 사진의 대웅과 시은의 모습"></img>
                 </figure>
 
-                <p>
+                <p className="btm-txt highlight">
                     축하해주신 모든분들께<br/>감사드립니다.
                 </p>
             </div>
--- my/src/section/intro.jsx
+++ my/src/section/intro.jsx
@@ -4,7 +4,7 @@ import introImg from '../img/img-intro.png'
 function Intro(props){
     return(
         <section className="section_intro">
-            <h1>대웅과 시은<br/>결혼합니다</h1>
+            <h1 className="highlight">대웅과 시은<br/>결혼합니다</h1>
             <img src={introImg} alt="무릎을 맞대고 앉아 정면을 보고 웃는 대웅과 시은"></img>
             <p>2024. 12. 08 | 11:10 <span>라비니움 1층 리츄얼홀</span></p>
         </section>
